003185003a2a68d9122a8e3d97a451a9ff578926
Yusuf Najmiddinov <najmiddinovyusuf3@gmail.com>
2023-08-08 11:13:07 +0300

    first commit

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/Rectangle-1.png';
+import blog02 from '../../assets/Rectangle-2.png';
+import blog03 from '../../assets/Rectangle-3.png';
+import blog04 from '../../assets/Rectangle-4.png';
+import blog05 from '../../assets/Rectangle-5.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+}
